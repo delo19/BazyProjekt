698931d771094c1319e18b09fa15589d80a7c1fc
TomaszCegielka <creativetomek@gmail.com>
2013-12-29 01:53:05 +0100

    dodanie grafiki do main page podstawowa walidacja hasla i loginu

--- Bazy projekt/Bazy projekt/MainWindow.xaml.cs
+++ Bazy projekt/Bazy projekt/MainWindow.xaml.cs
@@ -22,9 +22,57 @@ namespace Bazy_projekt
     {
         public MainWindow()
         {
-            InitializeComponent();
-			//treololo
-            //hhehehehehhehehehehheheehhe
+            InitializeComponent();			
+        }
+
+        private void ustawGwiazdki(object sender, TextChangedEventArgs e)
+        {
+            //TODO fokus na koncu ustawic
+            TextBox textBox = (TextBox)sender;
+            int liczbaZnakow = textBox.Text.Length;
+            String tmp = "";
+            for (int i = 0; i < liczbaZnakow; i++)
+            {
+                tmp += "*";
+            }
+            textBox.Text = tmp;
+        }
+
+        private void login(object sender, MouseButtonEventArgs e)
+        {
+            String allertMessage = this.checkLoginAndPassword(loginTextBox.Text, passwordBox.Password);
+            if (allertMessage.Equals(""))
+            {
+                alertMainWidnow.Text = allertMessage;
+                //TODO nawiguj dalej
+                _mainFrame.NavigationService.Navigate(new DashboardPage());
+            }else{
+                // cos poszlo nie tak
+                alertMainWidnow.Text = allertMessage;
+            }
+
+              /*
+               * Nawigacja dla Windows dzialajaca
+               * 
+              DashboardWindow w = new DashboardWindow();
+              w.Show();
+               * */
+        }
+
+        private string checkLoginAndPassword(String login, String haslo)
+        {
+            if (login.Length <= 4 || login.Length>30)
+            {
+                return "Login musi składać się przynajniej z 5 znaków a maksymalnie z 30";
+            }
+
+            if (haslo.Length <= 4 || haslo.Length > 30)
+            {
+                return "Haslo musi składać się przynajniej z 5 znaków a maksymalnie z 30";
+            }
+            //TODO sprawdzenie czy taki uzytkownik isteniej w bazie
+            //TODO sprawdzenie czy dla danego uzytkownika wpisano poprawne haslo
+            return "";
         }
     }
 }

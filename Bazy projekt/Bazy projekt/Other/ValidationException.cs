1fc68d337b442b41927af71a85d89aed67ae6038
delo19 <delo19>
2013-12-29 19:00:05 +0100

    Podpiecie rejestracji + poczatki walidacji

---
+++ Bazy projekt/Bazy projekt/Other/ValidationException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazy_projekt.Other
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException(String message) : base(message) { }
+    }
+}

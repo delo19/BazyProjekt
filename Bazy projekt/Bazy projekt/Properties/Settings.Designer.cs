79697f8627c7d2b4d73501c24b6c076fd0bd9e2e
del19@wp.pl <del19@wp.pl>
2014-01-15 11:50:33 +0100

    Konczenie zmian

--- Bazy projekt/Bazy projekt/Properties/Settings.Designer.cs
+++ Bazy projekt/Bazy projekt/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace Bazy_projekt.Properties {
                 return ((string)(this["Projekt1_14ConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\bin\\Debug\\Projekt1." +
+            "14.accdb")]
+        public string Model {
+            get {
+                return ((string)(this["Model"]));
+            }
+        }
     }
 }
